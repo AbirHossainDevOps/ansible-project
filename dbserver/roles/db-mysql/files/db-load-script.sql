50d7a9836701c4c61b61a20cecfa301be5b85269
Abir Hossain <abir.hossain@bjitacademy.com>
2023-05-26 08:02:01 +0600

    added images folder

--- dbserver/roles/db-mysql/files/db-load-script.sql
+++ dbserver/roles/db-mysql/files/db-load-script.sql
@@ -2,34 +2,6 @@ GRANT ALL PRIVILEGES ON *.* TO 'librarian'@'192.168.20.135' IDENTIFIED BY
 'librarypass' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 USE library;
--- phpMyAdmin SQL Dump
--- version 5.0.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 28, 2021 at 06:07 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.34
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `library`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
 
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
@@ -259,8 +231,3 @@ ALTER TABLE `tblissuedbookdetails`
 ALTER TABLE `tblstudents`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
